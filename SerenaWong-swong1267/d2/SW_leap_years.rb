ea974604bd4264310b1cc60c23fd96986deba777
Serena Wong <serenaw@stanford.edu>
2015-07-14 15:49:10 +0200

    Finished leap years

--- SerenaWong-swong1267/d2/SW_leap_years.rb
+++ SerenaWong-swong1267/d2/SW_leap_years.rb
@@ -5,26 +5,15 @@ finish = gets.to_i
 puts"Leap Years: "
 
 curr_year = start;
-leap_year = false;
 
-while !leap_year
-	if curr_year % 4 == 0
-		leap_year = true
+while curr_year <= finish
+	if curr_year % 100 == 0 && curr_year % 400 == 0 || curr_year % 4 == 0 && curr_year % 100 != 0
+		puts "#{curr_year}"
+		curr_year += 4
 	else
 		curr_year += 1
 	end
 end
-print "#{curr_year}"
-curr_year += 4
 
-while curr_year <= finish
-	if curr_year % 100 == 0 && curr_year % 400 == 0
-		print ", #{curr_year}"
-	else
-		print ", #{curr_year}"
-	end
-	curr_year += 4
-end
-print "\n"
 
 		
