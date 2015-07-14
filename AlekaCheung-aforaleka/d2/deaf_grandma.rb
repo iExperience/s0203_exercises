c897ca86264466e0973c5405f23ad43bc95eb8f3
aforaleka <aforaleka@gmail.com>
2015-07-14 16:36:09 +0200

    updated the deaf grandma to be more clingy

--- AlekaCheung-aforaleka/d2/deaf_grandma.rb
+++ AlekaCheung-aforaleka/d2/deaf_grandma.rb
@@ -1,12 +1,21 @@
 puts "say something to grandma: "
 words = gets
-year = rand(0...20)
+number = 3
+
 while words.strip != "BYE"
 	if words == words.upcase
-		puts "NO, NOT SINCE #{year + 1930}"
+		puts "NO, NOT SINCE #{rand(0...20) + 1930}"
 		words = gets
 	else
 		puts "HUH?! SPEAK UP, SONNY!"
 		words = gets
+	end
+end
+
+if words.strip == "BYE"
+	words = gets
+	if words.strip == "BYE"
+		words = gets
+		puts "I'm so disappointed T_T.. bye.."
 	end
 end
