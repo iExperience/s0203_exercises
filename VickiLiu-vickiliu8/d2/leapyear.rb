dad00d160464cf6b30223a45fb63429c9a5f85d1
vickiliu8 <vickiliu@umich.edu>
2015-07-14 09:29:36 -0400

    all

---
+++ VickiLiu-vickiliu8/d2/leapyear.rb
@@ -0,0 +1,15 @@
+puts "enter starting year"
+syear = gets.to_i
+puts "enter ending year"
+eyear = gets.to_i
+
+puts "these are leap years"
+while syear <= eyear 
+if (syear%4 == 0 && syear%100 != 0)
+	puts syear
+	
+end
+
+
+ syear += 1
+end
