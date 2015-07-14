05efa870f900d206a643252b1d7a774fbbe2fdf6
tkaye407 <tkaye@princeton.edu>
2015-07-14 11:22:50 +0200

    added greeting program

---
+++ TylerKaye-tkaye407/d2/tyler_kaye_greeting.rb
@@ -0,0 +1,7 @@
+print "What is your first name: "
+fname = gets.chomp.capitalize
+print "What is your middle name: "
+mname = gets.chomp.capitalize
+print "What is your last name: "
+lname = gets.chomp.capitalize
+puts "Hello " + fname + " " + mname + " " + lname
