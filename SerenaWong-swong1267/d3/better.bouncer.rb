8ff1cb89c4f974068c86820d1260647797a6b42d
Serena Wong <serenaw@stanford.edu>
2015-07-15 13:20:22 +0200

    Updated better_bouncer.rb

--- SerenaWong-swong1267/d3/better.bouncer.rb
+++ SerenaWong-swong1267/d3/better.bouncer.rb
@@ -14,10 +14,11 @@ end
 def strict_bouncer(people, letter)
 	allowed_in = []
 	people.each do |person|
-		if person[0][0] != letter && person[1] >= 21
+		if person[0][0].downcase != letter.downcase && person[1] >= 21
 			allowed_in.push person[0]
 		end
 	end
 	allowed_in
 end
 
+puts strict_bouncer([['erica', 22], ['ian', 24], ['brian', 34], ['seth', 18]], 'i').inspect
