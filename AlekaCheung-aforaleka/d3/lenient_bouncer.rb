7c522de1872975525b55ed135af7ee76efb48fa0
aforaleka <aforaleka@gmail.com>
2015-07-15 13:26:59 +0200

    changed lenient_bouncer, playing with .each

--- AlekaCheung-aforaleka/d3/lenient_bouncer.rb
+++ AlekaCheung-aforaleka/d3/lenient_bouncer.rb
@@ -24,34 +24,29 @@ def bouncer(age, country)
 	end
 end
 
-# printing the answers
+
+
+def strict_bouncer(people_array, letter)
+	allowed_list = Array.new
+
+	people_array.each do |people|
+		if (people[0][0] != letter) && (people[1] >= 21)
+			allowed_list << people[0]
+		end
+	end
+	allowed_list
+end
+
+# bouncer:
 # puts bouncer(18, 'South Africa')
 # puts bouncer(16, 'South Africa')
 # puts bouncer(21, 'USA')
 # puts bouncer(18, 'uSa')
 # puts bouncer(4, 'sfd')
 
-#this guy's having a bad day
-def strict_bouncer(array, letter)
-	count = 0
-	allowed_list = Array.new
-
-	while count < array.length
-		if array[count][0][0] == letter
-			count += 1
-		elsif array[count][1] < 21
-			count += 1
-		else
-			allowed_list << array[count][0]
-			count += 1
-		end
-	end
-	allowed_list
-end
-
-# printing the answers
-# puts strict_bouncer([['erica', 22], ['ian', 24], ['brian', 34], ['seth', 18]], 'i')
-# puts strict_bouncer([['aaron', 28], ['rafi', 21]], 'i')
+# strict bouncer:
+puts strict_bouncer2([['erica', 22], ['ian', 24], ['brian', 34], ['seth', 18]], 'i')
+puts strict_bouncer2([['aaron', 28], ['rafi', 21]], 'i')
 
 
 
