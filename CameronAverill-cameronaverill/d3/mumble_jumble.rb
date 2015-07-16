1173d890d4c0a8732f26c454100f06759172703a
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-16 18:08:34 +0200

    3A and 3B

--- CameronAverill-cameronaverill/d3/mumble_jumble.rb
+++ CameronAverill-cameronaverill/d3/mumble_jumble.rb
@@ -2,7 +2,10 @@ def jumble(str_to_jumble)
 	str_to_jumble.split('').shuffle.join('')
 end
 
+puts jumble('the dog')
 puts jumble('turtle')
+puts jumble('')
+puts jumble("Nice, Bru! Try to talk properly")
 
 def mumble(str_to_mumble)
 	str_to_mumble.downcase
