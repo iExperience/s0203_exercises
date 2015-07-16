dd41fc9226512929f48a33a5ba54bc44bdbe1d87
Serena Wong <serenaw@stanford.edu>
2015-07-16 16:46:41 +0200

    finished 4a exercises

--- SerenaWong-swong1267/d4/calculator_app/calc_app.rb
+++ SerenaWong-swong1267/d4/calculator_app/calc_app.rb
@@ -1,32 +1,26 @@
 require "sinatra"
 
 get "/" do
-	"This is a calulator app"
-	erb :index
+	"This is a calulator app. In the URL, type the operation you want to perform, followed by the two operands, all separated by slashes. This calculator can handle addition, subtraction, multiplication, division, and exponents between two operands."
 end
 
 get "/add/:num1/:num2" do |num1, num2|
-	@sum = num1 + num2
-	erb :index
+	"The sum of #{num1} and #{num2} is #{num1.to_i + num2.to_i}!"
 end
 
 get "/subtract/:num1/:num2" do |num1, num2|
-	@difference = num1 + num2
-	erb :index
+	"#{num1} minus #{num2} is #{num1.to_i - num2.to_i}!"
 end
 
 get "/multiply/:num1/:num2" do |num1, num2|
-	@product = num1 + num2
-	erb :index
+	"The product of #{num1} and #{num2} is #{num1.to_i * num2.to_i}!"
 end
 
 get "/divide/:num1/:num2" do |num1, num2|
-	@quotient = num1 + num2
-	erb :index
+	"#{num1} divided by #{num2} is #{num1.to_f / num2.to_f}!"
 end
 
 get "/expomnent/:num1/:num2" do |num1, num2|
-	@answer = num1 + num2
-	erb :index
+	"#{num1} raised to the #{num2} power is #{num1 ** num2}!"
 end
 
