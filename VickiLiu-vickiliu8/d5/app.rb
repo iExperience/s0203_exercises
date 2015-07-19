4a53671a0e17feba209e6dd955eb078fa46eb852
vickiliu8 <vickiliu@umich.edu>
2015-07-19 15:40:16 -0400

    stuff

--- VickiLiu-vickiliu8/d5/app.rb
+++ VickiLiu-vickiliu8/d5/app.rb
@@ -5,7 +5,8 @@ get "/" do
 	erb :home
 end
 
-get "/match/:index" do
+get "/match/:match_num/:avoid_num" do |match, avoid|
+
 	animals = [
    	 	{:description => "blah", :image => "blah", :name => "Giraffe"},
 		{:description => "blah", :image => "blah", :name => "Sponge"},
@@ -16,6 +17,10 @@ get "/match/:index" do
 		{:description => "blah", :image => "blah", :name => "Beefcake"},
 		{:description => "blah", :image => "blah", :name => "Quokka"}
 	]
+
+	@match_name = animals[match.to_i][:name]
+	@avoid_name = animals[avoid.to_i][:name]
+	erb :match_index
 end
 
 
@@ -32,8 +37,19 @@ animals = [
 	{:description => "blah", :image => "blah", :name => "Beefcake"},
 	{:description => "blah", :image => "blah", :name => "Quokka"}
 
-	@match_num = rand(8)
+	
 ]
+@match_num = rand(8)
+@avoid_num = rand(8)
+
+while @match_num == index.to_i 
+	@match_num = rand(8)
+
+end
+
+while (@avoid_num == @match_num || @avoid_num == index.to_i)
+	@avoid_num = rand(8)
+end
 
 @name = animals[index.to_i][:name] #to_i
 @description = animals[index.to_i][:description]
