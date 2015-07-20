648522363583f0048e28addf29cd91796ad6f544
vickiliu8 <vickiliu@umich.edu>
2015-07-20 05:45:18 -0400

    dun

--- VickiLiu-vickiliu8/d5/spiritanimal_app/app.rb
+++ VickiLiu-vickiliu8/d5/spiritanimal_app/app.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 
 get "/" do
-	@rand_num = rand(8)
+	@rand_num = rand(10)
 	erb :home
 end
 
@@ -15,7 +15,11 @@ get "/match/:match_num/:avoid_num" do |match, avoid|
 		{:description => "Whoa dude.  Chill out.  Maybe you should see a therapist or something for your anger issues.", :image => "baboon.jpg", :name => "Angry Baboon"},
 		{:description => "You're the kinda dude who gets knocked down by a wave and pops right back up.", :image => "penguin.jpg", :name => "Derpy Penguin"},
 		{:description => "*various catcalling noises*  You definitely know how to work your assets.", :image => "beefcake.jpg", :name => "Beefcake"},
-		{:description => "Always happy.  Like, maybe a little too happy.", :image => "quokka.jpg", :name => "Quokka"}
+		{:description => "Always happy.  Like, maybe a little too happy.", :image => "quokka.jpg", :name => "Quokka"},
+		{:description => "Interests include: factories, fast food, farming and being awesome", :image => "brian.jpg", :name => "Brian"},
+		{:description => "you sicken me.", :image => "decaf-coffee.jpg", :name => "Decaf Coffee"}
+
+
 	]
 
 	@match_name = animals[match.to_i][:name]
@@ -37,18 +41,20 @@ animals = [
 		{:description => "Whoa dude.  Chill out.  Maybe you should see a therapist or something for your anger issues.", :image => "baboon.jpg", :name => "Angry Baboon"},
 		{:description => "You're the kinda dude who gets knocked down by a wave and pops right back up.", :image => "penguin.jpg", :name => "Derpy Penguin"},
 		{:description => "*various catcalling noises*  You definitely know how to work your assets.", :image => "beefcake.jpg", :name => "Beefcake"},
-		{:description => "Always happy.  Like, maybe a little too happy.", :image => "quokka.jpg", :name => "Quokka"}
+		{:description => "Always happy.  Like, maybe a little too happy.", :image => "quokka.jpg", :name => "Quokka"},
+		{:description => "Interests include: factories, fast food, farming and being awesome", :image => "brian.jpg", :name => "Brian"},
+		{:description => "you sicken me.", :image => "decaf-coffee.jpg", :name => "Decaf Coffee"}
 	]
-@match_num = rand(8)
-@avoid_num = rand(8)
+@match_num = rand(10)
+@avoid_num = rand(10)
 
 while @match_num == index.to_i 
-	@match_num = rand(8)
+	@match_num = rand(10)
 
 end
 
 while (@avoid_num == @match_num || @avoid_num == index.to_i)
-	@avoid_num = rand(8)
+	@avoid_num = rand(10)
 end
 
 @name = animals[index.to_i][:name] #to_i
