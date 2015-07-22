2d8801a2ce0bf828b56fc3dde205f63699d93c57
Serena Wong <serenaw@stanford.edu>
2015-07-22 14:55:26 +0200

    added validations for animals

--- SerenaWong-swong1267/d8/cheetah_watch/app/models/animal.rb
+++ SerenaWong-swong1267/d8/cheetah_watch/app/models/animal.rb
@@ -1,2 +1,5 @@
 class Animal < ActiveRecord::Base
+  validates :name, :species, :color, :region, presence: true
+  validates :size, :age, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :diet, inclusion: { in: ["omnivore", "carnivore", "herbivore"] }
 end
