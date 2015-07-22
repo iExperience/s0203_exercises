18061ba5d848187ba72058bf8d5e2a965b9cc3bd
menachemdc <menachemdc@gmail.com>
2015-07-22 11:02:14 +0200

    Added Pattern

--- MenachemShemtov-menachemdc/d8/cheetah_watch/db/schema.rb
+++ MenachemShemtov-menachemdc/d8/cheetah_watch/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722083710) do
+ActiveRecord::Schema.define(version: 20150722085540) do
 
   create_table "animals", force: :cascade do |t|
     t.string   "species"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150722083710) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "pattern"
   end
 
 end
