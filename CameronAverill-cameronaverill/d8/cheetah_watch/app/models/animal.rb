491e0ba06385aa67b3d30da2be92c1d171d9e1c5
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-22 14:55:06 +0200

    added validations to an animal

--- CameronAverill-cameronaverill/d8/cheetah_watch/app/models/animal.rb
+++ CameronAverill-cameronaverill/d8/cheetah_watch/app/models/animal.rb
@@ -1,2 +1,6 @@
 class Animal < ActiveRecord::Base
+	validates :name, :species, presence: true
+	validates :size, numericality: {greater_than_or_equal_to: 0, 
+		only_integer: true}
+		validates :diet, inclusion: {in: ["omnivore", "herbivore", "carnivore"]}
 end
