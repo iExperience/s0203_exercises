4a1b165109a32715edb5969db0d186ce6009389d
menachemdc <menachemdc@gmail.com>
2015-07-22 11:08:21 +0200

    Removed pattern

--- MenachemShemtov-menachemdc/d8/cheetah_watch/db/schema.rb
+++ MenachemShemtov-menachemdc/d8/cheetah_watch/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722085540) do
+ActiveRecord::Schema.define(version: 20150722090324) do
 
   create_table "animals", force: :cascade do |t|
     t.string   "species"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20150722085540) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "pattern"
   end
 
 end
