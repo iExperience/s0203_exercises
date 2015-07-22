325d8bf902115536202dc1e155f0a7f573b30057
tkaye407 <tkaye@princeton.edu>
2015-07-22 10:14:28 +0200

    finished up d6 stuff

--- TylerKaye-tkaye407/d6/blog/app/controllers/articles_controller.rb
+++ TylerKaye-tkaye407/d6/blog/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-	def index
+	http_basic_authenticate_with name: "tak", password: "tkaye", except: [:index, :show]
+
+  def index
     	@articles = Article.all
  	end
 
