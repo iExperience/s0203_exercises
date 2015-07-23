6026221f83ff204f7fb8ab955df4892031d5b138
Aobo Guo <aobo.guo@gmail.com>
2015-07-23 14:48:10 +0200

    Added create and update forms for animals

--- AoboGuo-aoboguo/projects/cheetah_watch/app/controllers/animals_controller.rb
+++ AoboGuo-aoboguo/projects/cheetah_watch/app/controllers/animals_controller.rb
@@ -10,4 +10,38 @@ class AnimalsController < ApplicationController
 		@animal = Animal.find(params[:id])
 	end
 
+	def new
+		@animal = Animal.new 
+	end
+
+	def create
+		@animal = Animal.new(animal_params)
+
+		if @animal.save
+			redirect_to animal_path(@animal)
+		else 
+			render :new
+		end
+	end 
+
+	def edit
+		@animal = Animal.find(params[:id])
+	end
+
+	def update
+		@animal = Animal.find(params[:id])
+
+		if @animal.update(animal_params)
+			redirect_to animal_path(@animal)
+		else
+			render :edit
+		end
+	end
+
+private 
+	def animal_params
+		animal_params = params.require(:animal).permit(:name, :species, :region, :diet, :color, :age, :size)
+	end 
+
+
 end
