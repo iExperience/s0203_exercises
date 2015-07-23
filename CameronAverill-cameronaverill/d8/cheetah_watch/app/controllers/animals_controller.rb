891f6af0818c9b2ffa7298004f2e1d2ab7217740
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-23 11:18:39 +0200

    lots of stuff

--- CameronAverill-cameronaverill/d8/cheetah_watch/app/controllers/animals_controller.rb
+++ CameronAverill-cameronaverill/d8/cheetah_watch/app/controllers/animals_controller.rb
@@ -7,4 +7,39 @@ class AnimalsController < ApplicationController
 		@animal = Animal.find(params[:id])
 	end
 
+	def new
+		@animal = Animal.new
+	end
+
+	def create
+		@animal = Animal.new(animal_params)
+
+		@animal.save
+
+		if @animal.save
+			redirect_to animal_path(@animal)
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@animal = Animal.find(params[:id])
+	end
+
+	def update
+		@animal = Animal.find(params[:id])
+
+		if @animal.update(animal_params)
+			redirect_to animal_path(@animal)
+		else
+			render :edit
+		end 
+	end
+
+	private
+		def animal_params
+			params.require(:animal).permit(:name, :species, :region,
+				:diet, :age, :color, :size)
+		end
 end
