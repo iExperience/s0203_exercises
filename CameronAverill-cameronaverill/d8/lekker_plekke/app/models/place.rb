b24b37323928e4d53bb7a53e6fb56d6c6836ed90
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-23 22:36:25 +0200

    added comments

--- CameronAverill-cameronaverill/d8/lekker_plekke/app/models/place.rb
+++ CameronAverill-cameronaverill/d8/lekker_plekke/app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ActiveRecord::Base
 	validates :name, :address, :rating, presence: true
 	validates :rating, numericality: {only_integer: true, less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
-	has_many :comments
+	has_many :comments, dependent: :destroy
 end
